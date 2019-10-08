1474b114734c01ba3f58a2de0029d924d3f1757d
Alvin Garcia <alving@sourcepad.com>
2019-10-08 17:45:55 +0800

    pessimistic check db connection to avoid disconnect error - will need to monitor if this is resource-expensive

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,14 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from config import Config
+
+from flask_sqlalchemy import SQLAlchemy as _BaseSQLAlchemy
+
+class SQLAlchemy(_BaseSQLAlchemy):
+    def apply_pool_defaults(self, app, options):
+        super(SQLAlchemy, self).apply_pool_defaults(app, options)
+        options['pool_pre_ping'] = True
+
+db = SQLAlchemy()
 
 app = Flask(__name__)
 app.config.from_object(Config)
